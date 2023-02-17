970c58d1ce1191112a6021cdec6f4cff52f324ad
sjoleee <cent7425@gmail.com>
2023-02-18 01:13:56 +0900

    feat: tailwind custom colors 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,39 @@
 
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          200: "#6B89FF",
+          400: "#365fff",
+          600: "#0000bf",
+        },
+        secondary: {
+          300: "#93e594",
+          400: "#00d238",
+          600: "#007400",
+        },
+        tertiary: {
+          500: "#2B3752",
+          700: "#1F2231",
+          900: "#0F0F12",
+        },
+        neutral: {
+          0: "#2B3752",
+          300: "#E7E7E7",
+          500: "#9A9A9A",
+          700: "#3D3D3D",
+          900: "#000000",
+        },
+        system: {
+          success: "#54C60E",
+          information: "#0095F6",
+          warning: "#FF7E12",
+          error: "#FD3D51",
+        },
+      },
+    },
+  },
   plugins: [],
 };

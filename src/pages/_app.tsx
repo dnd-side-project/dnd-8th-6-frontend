99cc2768020bb30b402e5185fba3ead84d4e84ef
devryyeong <thisisme20@daum.net>
2023-02-19 03:00:41 +0900

    feat: 카카오 로그인 버튼 컴포넌트 작성

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
+import { useEffect } from "react";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    try {
+      if (!window.Kakao.isInitialized() && window.Kakao) {
+        window.Kakao.init(process.env.KAKAO_KEY);
+      }
+    } catch(e) { console.log(e)}
+
+  }, [])
+
+  return (
+    <>
+    <Component {...pageProps} />
+    </>
+  )
 }
---
+++ src/pages/login/KakaoButton.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import KakaoSymbol from '@assets/kakao.svg'
+
+const { KAKAO_OAUTH_URL, KAKAO_REST_API_KEY } = process.env
+const KAKAO_REDIRECT_URL = `${KAKAO_OAUTH_URL}/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${window.location.origin}/login/KAKAO&response_type=code`
+
+export default function KakaoButton() {
+  const handleKakaoLogin = () => {
+    try {
+      window.location.replace(KAKAO_REDIRECT_URL)
+    } catch (e) {
+      alert('현재 카카오 로그인이 불가합니다. 나중에 다시 시도해주세요.')
+    }
+  }
+
+  return (
+    <button 
+      aria-label="kakao-login-button" 
+      className="flex h-12 w-full items-center rounded-md border-none bg-kakao px-5 text-sm font-semibold text-gray-10 hover:cursor-pointer" 
+      onClick={handleKakaoLogin}>
+      <KakaoSymbol
+        aria-label="kakao-symbol-login"
+        className="h-[18px] w-[18px]"
+      />
+      <p className="w-full text-gray-10">카카오 로그인</p>
+    </button>
+  )
+}

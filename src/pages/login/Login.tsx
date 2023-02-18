580a497d3dd9c814af172dc6efbd060c2ab11d81
devryyeong <thisisme20@daum.net>
2023-02-19 05:07:03 +0900

    feat: login 페이지 추가

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,7 +3,12 @@ import KakaoButton from './KakaoButton';
 import GithubButton from './GithubButton';
 
 const Login = () => {
-
+  return (
+    <div>
+      <KakaoButton />
+      <GithubButton />
+    </div>
+  )
 }
 
 export default Login;

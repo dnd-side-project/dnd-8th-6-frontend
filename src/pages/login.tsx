f030616a2421376c9873edc299507ec191050088
devryyeong <thisisme20@daum.net>
2023-02-22 02:23:09 +0900

    fix: declarations.d.ts 추가

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next'
 import KakaoButton from '../components/KakaoButton';
 import GithubButton from '../components/GithubButton';
 
-const Login : NextPage<Props> = (props) => {
+const Login : NextPage = () => {
   return (
     <div>
       <KakaoButton />

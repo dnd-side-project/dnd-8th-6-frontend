96020781ecaa83d2d0aef0b2a332b5a10e21312f
devryyeong <thisisme20@daum.net>
2023-03-01 05:54:01 +0900

    feat: focused style 설정

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -20,50 +20,61 @@ const searchConfig = {
   },
 };
 
-const SearchBar = forwardRef<HTMLInputElement, Props>(({ buttonType = "cancel", onClick }, ref) => {
-  const [isFocused, setIsFocused] = useState(true);
-  const searchDiv = useRef<HTMLInputElement>(null);
+const SearchBar = forwardRef<HTMLInputElement, Props>(
+  ({ buttonType = "cancel", onClick }, ref) => {
+    const [isFocused, setIsFocused] = useState<boolean>(false);
+    const [isFilled, setIsFilled] = useState<boolean>(false);
+    const searchDiv = useRef<HTMLInputElement>(null);
 
-  const handleFocus = () => {
-    searchDiv.current?.focus();
-    setIsFocused(!isFocused);
-  };
+    const onFocus = () => {
+      searchDiv.current?.focus();
+      setIsFocused(!isFocused);
+    };
 
-  const handleBlur = () => {
-    searchDiv.current?.blur();
-    setIsFocused(!isFocused);
-  };
+    const onBlur = () => {
+      searchDiv.current?.blur();
+      setIsFocused(!isFocused);
+    };
 
-  return (
-    <div className="flex bg-tertiary-700 w-full px-4 py-3">
-      <div ref={searchDiv} className="opacity-50 bg-tertiary-500 rounded-md">
+    const onChange = () => {
+      setIsFilled(!isFilled);
+    };
+
+    return (
+      <div className="flex bg-tertiary-700 w-full px-4 py-3">
         <div
-          className="flex items-center p-2 border-primary-400"
-          onFocus={handleFocus}
-          onBlur={handleBlur}
+          ref={searchDiv}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          className={`bg-tertiary-500/50 rounded-md ${
+            isFocused ? "border border-primary-400" : ""
+          }`}
         >
-          {isFocused ? (
-            <ActiveSearchIcon className="mr-2" />
-          ) : (
-            <InactiveSearchIcon className="mr-2" />
-          )}
-          <input
-            className="w-full outline-none text-neutral-0 bg-transparent"
-            type="text"
-            id="search"
-            placeholder="내용 입력"
-            ref={ref}
-          />
+          <div className="flex items-center p-2 gap-2">
+            {isFocused || isFilled ? (
+              <ActiveSearchIcon className="mr-2 w-6" />
+            ) : (
+              <InactiveSearchIcon className="mr-2 w-6" />
+            )}
+            <input
+              className="w-full outline-none text-neutral-0 bg-transparent"
+              type="text"
+              id="search"
+              placeholder="내용 입력"
+              ref={ref}
+              onChange={onChange}
+            />
+          </div>
         </div>
+        <button onClick={onClick}>
+          <T2 className={`px-4 text-neutral-0 ${searchConfig[buttonType].textColor}`}>
+            {searchConfig[buttonType].text}
+          </T2>
+        </button>
       </div>
-      <button onClick={onClick}>
-        <T2 className={`px-4 text-neutral-0 ${searchConfig[buttonType].textColor}`}>
-          {searchConfig[buttonType].text}
-        </T2>
-      </button>
-    </div>
-  );
-});
+    );
+  },
+);
 
 SearchBar.displayName = "SearchBar";
 export default SearchBar;

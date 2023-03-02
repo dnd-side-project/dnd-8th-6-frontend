9f861cc069eb14da278ab37f10c0faadb0378c8c
devryyeong <thisisme20@daum.net>
2023-03-03 03:06:50 +0900

    feat: msw 셋팅

---
+++ src/mocks/api/data/githubStatsData.ts
@@ -0,0 +1,14 @@
+/**
+ * API 호출 시 응답받을 데이터를 설정합니다.
+ */
+export const githubStatsData = {
+  data: [
+    {
+      stars: "2200",
+      commits: "2200",
+      PRs: "2200",
+      issues: "2200",
+      contributedTo: "2200",
+    },
+  ],
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,12 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Noto_Sans_KR } from "@next/font/google";
 
+import initMockAPI from "@/mocks";
+
+if (process.env.NODE_ENV === "development") {
+  initMockAPI();
+}
+
 const notoSansKr = Noto_Sans_KR({
   weight: ["100", "300", "400", "500", "700", "900"],
   subsets: ["latin"],

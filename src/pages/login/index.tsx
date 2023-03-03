ec4437a49869638ccb5e8c97e49eb4b11ae3fd50
sjoleee <cent7425@gmail.com>
2023-03-04 04:53:37 +0900

    fix: oauth 기능 수정

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { NextPage } from "next";
+
+import KakaoButton from "@/components/KakaoButton";
+import GithubButton from "@/components/GithubButton";
+
+const Login: NextPage = () => {
+  return (
+    <div>
+      <KakaoButton />
+      <GithubButton />
+    </div>
+  );
+};
+
+export default Login;
